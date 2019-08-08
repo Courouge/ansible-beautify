db429c6a2fbfbec834fb5831c0327ebb0c13a228
courouge_a@prodgrp.local <florian.courouge-prestataire@ca-silca.fr>
2019-08-08 16:13:27 +0200

    one liner killer is done :)

---
+++ README.md
@@ -0,0 +1,5 @@
+### Ansible One Liner Killer ###
+Support all module except shell module
+If you have your own library, no problem add it on the modules.txt file :)
+
+You can specify your own rolepath with option yolo :)
--- onelinerkiller.py
+++ onelinerkiller.py
@@ -2,29 +2,31 @@
 
 # coding: utf-8
 import os
+import argparse
+
 from ansible.errors import AnsibleParserError
 from ansible.parsing.mod_args import ModuleArgsParser
+from fnmatch import fnmatch
 
 ### list ansible modules ###
-
 f = open("modules.txt", "r")
 listmodules=[]
 for x in f:
   listmodules.append(x[:-1])
 f.close()
 
-### get *.yml form directory ###
 
-from fnmatch import fnmatch
-
+### get *.yml form role directory ###
 ymlfiles=[]
-dir = os.getcwd() + "/roles"
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Clean Ansible OneLiner in given path")
+args = parser.parse_args()
+# dir = os.getcwd() + "/roles"
+dir = args.path
 pattern = "*.yml"
 
 ## load all modules name in list ##
 listmodules = [line.rstrip('\n') for line in open("modules.txt")]
-
-lol = ["  " + s + ":" for s in listmodules]
 
 for path, subdirs, files in os.walk(dir):
     for name in files:
@@ -32,40 +34,37 @@ for path, subdirs, files in os.walk(dir):
             ymlfiles.append(os.path.join(path, name))
 
 ### open all *.yml ###
-
 for yml in ymlfiles:
   f = open(yml, "r")
   composefile = []
   flag = 0
   for x in f:
-
     for e in listmodules:
-
       if x.startswith("  " + e + ":") == True and len(x.strip()) > len(e + ":"):
         module = x.split(':',1 )[0].strip()
         arguments = x.split(':',1 )[1]
-        flag == 1
         composefile.append("  "+ module + ":\n")
         res = dict()
+        flag = 1
         res[module] = arguments
         m = ModuleArgsParser(res)
         mod, args, to = m.parse()
         for x in args:
-            if args[x].find('{{') != -1:
-                composefile.append("    "+ x + ": " + '"' + args[x] + '"' + "\n")
-                #print args[x]
+            if x == "_raw_params" :
+                composefile.append("    " + args[x] + "\n")
             else:
-               composefile.append("    "+ x + ": " + args[x] + "\n")
-
-    if flag == 1: #or x.startswith('#') or x.startswith('-'):
-        flag == 0
-        composefile.append(x)
-
+                if args[x].find('{{') != -1 and args[x].find('}}') != -1 :
+                    composefile.append("    "+ x + ": " + '"' + args[x] + '"' + "\n")
+                else:
+                    composefile.append("    "+ x + ": " + args[x] + "\n")
+        break
+    composefile.append(x)
+    if flag == 1:
+        flag = 0
+        composefile.pop(len(composefile)-1)
   f.close()
-  print composefile
-'''
   with open(yml, 'w') as f:
       for item in composefile:
           f.write("%s" % item)
   f.close()
-'''
+print "done"
