411474f0fbab81948307f452f27a45f1bd6df24b
courouge_a@prodgrp.local <florian.courouge-prestataire@ca-silca.fr>
2019-08-13 17:11:39 +0200

    first

--- web/app.py
+++ web/app.py
@@ -1,9 +1,23 @@
-From flask import Flask
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# App config.
+DEBUG = True
 app = Flask(__name__)
 
+class ProcessForm(Form):
+    submit_button = SubmitField('Do Something')
+
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def test():
+    form = ProcessForm()
+    if form['submit_button'] == 'Do Something':
+         if 'download' in request.form:
+            print("go")
+
+    else:
+        return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+   app.run(debug = True)
